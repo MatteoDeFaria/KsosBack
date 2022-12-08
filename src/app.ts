7939741b91b6bcbfbbcf307c5fde7c2b5495e206
Matteo <matteo@desktop-r7qhfk8.home>
2022-12-08 19:40:49 +0100

    [update]: remove hostname

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,12 @@ const initApp = async () => {
   const app = express();
 
   app.set("port", process.env.PORT || 8000);
-  app.set("hostname", process.env.HOSTNAME);
+//  app.set("hostname", process.env.HOSTNAME || "0.0.0.0");
   app.set("prefix", "/api");
 
   app.use(express.json());
 
-  app.use('/api', await routeBuilder());
+  app.use("/api", await routeBuilder());
 
   return app;
 };
--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import { Logger } from "./config/logger.config";
 
 appInit().then((app) => {
   const logger = Logger.Instance;
-  app.listen(app.get("port"), app.get("hostname"), () => {
+  app.listen(app.get("port"), () => {
     logger.info(
-      `Started on ${app.get("hostname")}:${app.get("port")}${app.get("prefix")}`
+      `Started on :${app.get("port")}${app.get("prefix")}`
     );
   });
 });
